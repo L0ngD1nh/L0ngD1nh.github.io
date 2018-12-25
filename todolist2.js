a3449bfc95fb6bbb19de1b12e1885f9dd1809290
Long Dinh <ld.thienvu@gmail.com>
2018-12-24 20:38:54 -0500

    todolist2change

---
+++ todolist2.js
@@ -0,0 +1,14 @@
+
+var lis = document.querySelectorAll("li");
+
+for (var i = 0; i < lis.length; i++) {
+	lis[i].addEventListener("mouseover", function () {
+		this.classList.add("pinky");
+	});
+	lis[i].addEventListener("mouseout", function () {
+		this.classList.remove("pinky");
+	});
+	lis[i].addEventListener("click", function () {
+		this.classList.toggle("done");
+	});
+}
